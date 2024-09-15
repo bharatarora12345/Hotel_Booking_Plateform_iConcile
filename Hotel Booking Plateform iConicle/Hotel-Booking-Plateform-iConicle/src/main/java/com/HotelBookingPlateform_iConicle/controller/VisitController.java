10d89d2f73790a3a19b6f0c8ca14c3db176f7821
Bharat Arora <arorabharat91@gmail.com>
2024-09-15 22:34:19 +0530

    complte the backend except recommendation system

---
+++ Hotel Booking Plateform iConicle/Hotel-Booking-Plateform-iConicle/src/main/java/com/HotelBookingPlateform_iConicle/controller/VisitController.java
@@ -0,0 +1,35 @@
+package com.HotelBookingPlateform_iConicle.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.HotelBookingPlateform_iConicle.entity.Hotel;
+import com.HotelBookingPlateform_iConicle.entity.User;
+import com.HotelBookingPlateform_iConicle.service.HotelService;
+import com.HotelBookingPlateform_iConicle.service.UserService;
+import com.HotelBookingPlateform_iConicle.service.VisitService;
+
+public class VisitController {
+	 @Autowired
+	 private VisitService visitService;
+	 
+	 @Autowired
+	 private UserService userService;
+	 
+	 @Autowired
+	 private HotelService hotelService;
+
+    @PostMapping("/track")
+    public ResponseEntity<String> trackVisit(@RequestParam Long userId, @RequestParam Long hotelId) {
+        // Assuming you have a service to get user and hotel by ID
+        User user = userService.getUserById(userId);
+        Hotel hotel = hotelService.getHotelById(hotelId);
+        visitService.trackVisit(user, hotel);
+        return ResponseEntity.ok("Visit tracked successfully");
+    }
+    
+    
+    
+}
