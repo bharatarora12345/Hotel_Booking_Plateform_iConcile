5860aa6b71a392b1916f18de9f60cbf18e5ee1e5
Bharat Arora <arorabharat91@gmail.com>
2024-09-15 13:09:36 +0530

    first commit demo project added

---
+++ Hotel Booking Plateform iConicle/Hotel-Booking-Plateform-iConicle/src/main/java/com/HotelBookingPlateform_iConicle/Hotel/Booking/Plateform/iConicle/HotelBookingPlateformIConicleApplication.java
@@ -0,0 +1,13 @@
+package com.HotelBookingPlateform_iConicle.Hotel.Booking.Plateform.iConicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingPlateformIConicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingPlateformIConicleApplication.class, args);
+	}
+
+}
---
+++ Hotel Booking Plateform iConicle/Hotel-Booking-Plateform-iConicle/src/test/java/com/HotelBookingPlateform_iConicle/Hotel/Booking/Plateform/iConicle/HotelBookingPlateformIConicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelBookingPlateform_iConicle.Hotel.Booking.Plateform.iConicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingPlateformIConicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
