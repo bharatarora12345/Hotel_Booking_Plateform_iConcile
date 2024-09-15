7cf7fff0788fae1008410c2d30cb6b58c2aecaa1
Bharat Arora <arorabharat91@gmail.com>
2024-09-15 14:13:13 +0530

    entity created
    1. Hotel
    2. User
    3. Booking

---
+++ Hotel Booking Plateform iConicle/Hotel-Booking-Plateform-iConicle/src/main/java/com/HotelBookingPlateform_iConicle/entity/Hotel.java
@@ -0,0 +1,49 @@
+package com.HotelBookingPlateform_iConicle.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private int rating;
+    private double pricePerNight;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public double getPricePerNight() {
+		return pricePerNight;
+	}
+	public void setPricePerNight(double pricePerNight) {
+		this.pricePerNight = pricePerNight;
+	}
+
+    
+}
